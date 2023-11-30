196e6ffd20c23fdf82f4df3350f1a8893afedafb
DoheumPark <pirbak@hanmail.net>
2023-11-30 19:12:57 +0900

    first

---
+++ src/main/java/com/green/growgreen/user/UserController.java
@@ -0,0 +1,14 @@
+package com.green.growgreen.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private UserService service;
+}
