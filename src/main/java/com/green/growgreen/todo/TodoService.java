0b0186c42c88d32d764ee2169d92932e43ae97ec
DoheumPark <pirbak@hanmail.net>
2023-11-30 21:20:41 +0900

    보충 3교시 todo 등록

---
+++ src/main/java/com/green/growgreen/todo/TodoService.java
@@ -0,0 +1,19 @@
+package com.green.growgreen.todo;
+
+import com.green.growgreen.ResVo;
+import com.green.growgreen.todo.model.TodoInsDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TodoService {
+    private final TodoMapper mapper;
+
+    public ResVo postTodo(TodoInsDto dto) {
+        int affectedRows = mapper.insTodo(dto);
+        return new ResVo(dto.getItodo());
+    }
+}
